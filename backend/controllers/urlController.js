d9701f7fcc7abce2bbc740e538479d4cdce7514a
Sampath107b <kadimichandu1234@gmail.com>
2025-09-08 22:27:41 +0530

    next

--- backend/controllers/urlController.js
+++ backend/controllers/urlController.js
@@ -135,17 +135,17 @@ const loginUser=async (req,res)=>{
         const {email,password}=req.body;
         if (!email || !password){
             res.status(400); 
-            throw new Error('Please provide an email and password');
+            return res.status(400).json({ success: false, error: 'Please provide an email and password' });
         }
         const user=await User.findOne({email}).select('+password');
         if (!user){
             res.status(401); // Unauthorized
-            throw new Error('Invalid credentials');
+            return res.status(401).json({ success: false, error: 'Invalid credentials' });
         }
         const isMatch=await bcrypt.compare(password,user.password);
         if (!isMatch){
             res.status(401); // Unauthorized
-            throw new Error('Invalid credentials');
+            return res.status(401).json({ success: false, error: 'Invalid credentials' });
         }
         
         const payLoad={
